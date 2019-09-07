b1d5601f069ca1341e84e06f9e2adb996daf1535
Rene Dohmen (imac) <acidjunk@gmail.com>
2019-09-08 01:03:46 +0200

    Updates

---
+++ precache-manifest.609ac73057d00f0b08032008ad4e48d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95bcf44397c779d6cb5c28f92c9c5075",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8739cc1d353edb22c15",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c0080b07d9395f443e50",
+    "url": "/static/js/2.fd3a0c70.chunk.js"
+  },
+  {
+    "revision": "e8739cc1d353edb22c15",
+    "url": "/static/js/main.51af5ef9.chunk.js"
+  },
+  {
+    "revision": "cffe4391aa43ee3f76a6",
+    "url": "/static/js/runtime~main.2a283978.js"
+  }
+]);
