54d3ddf938890d06c4aea3fa3668180ff513e849
Rene Dohmen <acidjunk@gmail.com>
2020-08-11 22:49:03 +0200

    many to many relations for strains + new product table + tabs + fix of some typo's

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,6 +1,8 @@
 import { ChromeReaderMode } from "@material-ui/icons";
 import React from "react";
 import {
+    BooleanField,
+    BooleanInput,
     Create,
     Datagrid,
     DisabledInput,
@@ -31,6 +33,7 @@ export const CategoryList = props => (
     <List {...props} filters={<CategoryFilter />} perPage="100">
         <Datagrid>
             <TextField source="name" />
+            <BooleanField source="cannabis" />
             <TextField source="shop_name" sortable={false} />
             <EditButton basePath="/categories" />
             <ShowButton basePath="/categories" />
@@ -47,6 +50,7 @@ export const CategoryEdit = props => (
         <SimpleForm>
             <DisabledInput source="id" />
             <TextInput source="name" validate={required()} />
+            <BooleanInput source="cannabis" />
             <TextField source="shop_name" />
         </SimpleForm>
     </Edit>
@@ -56,6 +60,7 @@ export const CategoryCreate = props => (
     <Create title="Create a Category" {...props}>
         <SimpleForm>
             <TextInput source="name" validate={required()} />
+            <BooleanInput source="cannabis" />
             <ReferenceInput source="shop_id" reference="shops" perPage={100} validate={required()}>
                 <SelectInput optionText="name" />
             </ReferenceInput>
