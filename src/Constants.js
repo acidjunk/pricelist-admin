0e292b454c98e1f6b404de9c307a698acb500bae
René Dohmen <acidjunk@gmail.com>
2022-10-28 17:15:18 +0200

    Bump version

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://127.0.0.1:8080";
+export const API_URL = "https://api.staging.prijslijst.info";
 export const IMAGE_URL = "https://d3sticxdmgvhkp.cloudfront.net";
-export const IMAGE_BUCKET = "images-prijslijst-info";
+export const IMAGE_BUCKET = "prijslijst-upload";
